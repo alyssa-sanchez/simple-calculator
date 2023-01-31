71a5e1d8ba3892a80ead347f9cfe9122acc88232
Alyssa Sanchez <sanchezaeinstein@gmail.com>
2023-01-31 21:32:46 +0800

    remove simple cal

--- app.js
+++
@@ -1,8 +0,0 @@
-let input = document.getElementById("input")
-let equal = document.getElementById("equal")
-let result = document.getElementById("result")
-
-equal.addEventListener("click", () => {
-	let answer = eval(input.value)
-	result.textContent += " " + answer
-})
